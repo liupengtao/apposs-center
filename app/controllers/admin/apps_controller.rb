cab5196488a15e8f89d3c84110dcefb7c11a1269
fsword <li.jianye@gmail.com>
2011-07-09 23:38:15 +0800

    1、简化 resource.class 调用 2、支持维护  app 时同时修改相关 machine（未完成）

--- app/controllers/admin/apps_controller.rb
+++ app/controllers/admin/apps_controller.rb
@@ -1,3 +1,9 @@
 class Admin::AppsController < InheritedResources::Base
   before_filter :authenticate_user!
+  
+  def update_resource(object, attributes)
+    attributes[:machines] = Machine.where(:id => attributes[:machines])
+    object.update_attributes(attributes)
+  end
+
 end
