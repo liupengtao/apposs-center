72a59bba91ea4be4801cf3d2789c1d2d5fb73c1a
fsword <li.jianye@gmail.com>
2011-07-13 22:25:57 +0800

    添加回调逻辑，补充缺少的字段

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
   def commands
-  	room = Room.find :first, :conditions => {:name => params[:room_name]}
+  	room = Room.where(:name => params[:room_name]).first
   	# 查询参数包括机房的 name 和 id，是考虑到 room 表的name字段发生变动，
   	# 此时应该谨慎处理，不下发相应的命令
   	render :text => Operation.inits.where(:room_id => room.id, :room_name => room.name).collect{|o|
@@ -9,11 +9,17 @@ class ApiController < ApplicationController
   	}.join("\n")
   end
   
+  #{host,Host},{oid,OperationId}
   def run
-  	render :text => 'run'
+    Operation.where(:id => params[:oid]).first.run
+    render :text => 'run'
   end
   
+  # {isok,atom_to_list(IsOk)},{host,Host},{oid,OperationId},{body,Body}
   def callback
-  	render :text => 'callback'
+    Operation.where(:id => params[:oid]).first.update_attributes(
+        :isok => params[:isok], :response => params[:body]
+    )
+  	render :text => 'ok'
   end
 end
