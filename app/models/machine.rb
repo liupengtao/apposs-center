47cb389b99a1bc8fc21918c6b928c6f6bcac1b7b
fsword <li.jianye@gmail.com>
2011-07-13 23:02:42 +0800

    显示返回结果

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,6 +2,6 @@ class Machine < ActiveRecord::Base
   belongs_to :room
   belongs_to :app
   
-  has_many :operations, :conditions => ['status <> ?', Operation::COMPLETED]
+  has_many :operations #, :conditions => ['status <> ?', Operation::COMPLETED]
   has_many :commands, :through => :operations
 end
