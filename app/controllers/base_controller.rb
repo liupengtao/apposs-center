29af75735657dc698e13cd891843e84dc3978045
fsword <li.jianye@gmail.com>
2011-07-20 11:58:23 +0800

    补充提交

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,7 +2,7 @@ class BaseController < ApplicationController
 
   before_filter :authenticate_user!
   
-  respond_to :xml
+  respond_to :json
 
   def current_app
     App.where(:id => 1).first||App.new
