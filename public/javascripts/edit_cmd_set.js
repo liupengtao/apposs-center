f5604713700ce8972c0285fb6784df91e72ce6c8
Liu Pengtao <lpt2008303483@yahoo.cn>
2011-07-22 14:52:51 +0800

    edit页面中的拖动还未完成。

--- public/javascripts/edit_cmd_set.js
+++ public/javascripts/edit_cmd_set.js
@@ -6,11 +6,10 @@
  * To change this template use File | Settings | File Templates.
  */
 Ext.onReady(function() {
-
     //请求获取命令组数据并解析
     var cmdGroupNodes = [];
     Ext.Ajax.request({
-        url:'/command_groups',
+        url:'/command_groups.json',
         callback:function(options, success, response) {
             var cmdGroups = Ext.decode(response.responseText);
             for (var i = 0; i < cmdGroups.length; i++) {
@@ -37,11 +36,9 @@ Ext.onReady(function() {
                 }
                 cmdGroupNodes[cmdGroupNodes.length] = cmdGroupNode;
             }
-            alert(cmdGroupNodes)
             var cmdGroupStore = Ext.create('Ext.data.TreeStore', {
                 root: {
-                    text: 'Ext JS',
-                    id: 'src',
+                    text: '命令组列表',
                     expanded: true,
                     children:cmdGroupNodes
                 },
@@ -54,15 +51,88 @@ Ext.onReady(function() {
                 ]
             });
 
+            //系统所有的命令组树
             var cmdGroupTreePanel = Ext.create('Ext.tree.Panel', {
                 title: '当前系统所有命令',
-//        rootVisible: false,
-                region:'center',
+                region:'west',
                 store:cmdGroupStore,
-                collapsible:true
+                collapsible:true,
+                viewConfig: {
+                    plugins: {
+                        ptype: 'treeviewdragdrop',
+                        allowParentInserts:true
+                    }
+                },
+                listeners:{
+                    beforeitemmove:function() {
+//                        cmdGroupStore.reload();
+                    }
+                }
+            });
+
+            for (var p in cmdGroupTreePanel.dragZone) {
+                alert(p + ':' + cmdGroupTreePanel.dragZone[p])
+            }
+
+            function $(id) {
+                return document.getElementById(id);
+            }
+
+            //获取命令包的相关信息
+            var appId = $('app_id').value;
+            var appName = $('app_name').value;
+            var cmdSetDefId = $('cmd_set_def_id').value;
+            var cmdSetDefName = $('cmd_set_def_name').value;
+            var cmdSetDefExpression = $('cmd_set_def_expression').options;
+
+            var cmdDefList = [];
+            for (var i = 0, len = cmdSetDefExpression.length; i < len; i++) {
+                var option = cmdSetDefExpression[i];
+                var cmdDef = {};
+                cmdDef.id = option.value;
+                cmdDef.text = option.text;
+                cmdDef.leaf = true
+                cmdDefList[cmdDefList.length] = cmdDef;
+            }
+
+            var cmdSetTreeStore = Ext.create('Ext.data.TreeStore', {
+                root: {
+                    text: cmdSetDefName,
+                    expanded: true,
+                    children:cmdDefList
+                },
+                folderSort: true,
+                sorters: [
+                    {
+                        property: 'text',
+                        direction: 'ASC'
+                    }
+                ]
+            });
+
+            //命令包树
+            var cmdSetTreePanel = Ext.create('Ext.tree.Panel', {
+                title:'命令包',
+                region:'center',
+                collapsible:true,
+                viewConfig: {
+                    plugins: {
+                        ptype: 'treeviewdragdrop',
+                        allowParentInserts:true
+                    }
+                },
+                store:cmdSetTreeStore,
+                listeners:{
+                    itemclick:function(view, record, item, index, e) {
+                        var root = cmdSetTreePanel.getRootNode();
+                        alert(root.firstChild.isFirst)
+                        alert(root.findChild('id',[1]).raw.text)
+                    }
+                }
             });
 
             var editCmdSetMainViewport = Ext.create('Ext.Viewport', {
+                renderTo:document.body,
                 layout: {
                     type: 'border',
                     padding: 5
@@ -71,9 +141,11 @@ Ext.onReady(function() {
                     split:true
                 },
                 items: [
-                    cmdGroupTreePanel
+                    cmdGroupTreePanel,
+                    cmdSetTreePanel
                 ]
             });
         }
     });
+
 });
